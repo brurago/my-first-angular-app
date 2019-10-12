7832f80885feb589c39a3b99d826edccd9e3d31c
Bruno Rocha Agostinho <brurago@gmail.com>
2019-10-12 15:10:03 -0300

    Estudo de Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component.';
+import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
